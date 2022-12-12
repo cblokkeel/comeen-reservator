adcb2cad0a98ac933297fe981d1c119f49a3bc57
cblokkeel <cblokkeel@partner.auchan.fr>
2022-12-12 22:17:21 +0100

    feat: add pages

---
+++ components/SelectTeam.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="w-full mt-12">
+    <form>
+      <input
+        type="text"
+        class="bg-gray-900 border-2 border-gray-700 rounded-lg w-full p-5 text-xl text-white"
+        placeholder="Votre équipe"
+      />
+    </form>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,11 @@
 <template>
-  <div class="wrapper"></div>
+  <div class="wrapper flex flex-col justify-center items-center h-screen">
+    <h1 class="text-white text-6xl font-bold">
+      Bienvenue sur Comeen Reservator
+    </h1>
+    <p class="text-lg text-gray-200 mt-4 italic">
+      Selectionnez votre équipe afin de réserver vos bureaux.
+    </p>
+    <SelectTeam />
+  </div>
 </template>
-
-<script setup lang="ts"></script>
-
-<style scoped></style>
